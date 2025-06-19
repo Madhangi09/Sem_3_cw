6a5b819ff9196e4a6bfd33ddb861074492e0d900
Madhangi <kamatchibala6@gmail.com>
2025-06-19 13:16:43 +0530

    Add files via upload

---
+++ jasc-1.js
@@ -0,0 +1,8 @@
+function light_ON()
+{
+document.getElementById('imgoff').src='https://i.pinimg.com/736x/5e/1a/2e/5e1a2e16e575c0d5bf433138c5351678.jpg';
+}
+function light_OFF()
+{
+document.getElementById('imgoff').src='https://thumbs.dreamstime.com/b/idea-bulb-turn-off-7354247.jpg';
+}
